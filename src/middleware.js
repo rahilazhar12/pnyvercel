938715b360933744ee27426502edb29c9afcc18e
Rahil Azhar <rahil.azhar10.com>
2024-08-19 11:38:24 +0500

    first commit

--- src/middleware.js
+++ src/middleware.js
@@ -4,11 +4,11 @@ export function middleware(req) {
   const { nextUrl: url } = req;
 
   // // Check if the URL contains a query string
-  // if (url.search) {
-  //   // Construct absolute URL for the redirect
-  //   const redirectUrl = new URL('/404', url.origin);
-  //   return NextResponse.redirect(redirectUrl);
-  // }
+  if (url.search) {
+    // Construct absolute URL for the redirect
+    const redirectUrl = new URL('/404', url.origin);
+    return NextResponse.redirect(redirectUrl);
+  }
 
   // Check if the URL pathname ends with .html or .txt
  
